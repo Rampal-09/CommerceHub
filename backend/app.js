566dbe159016fb6405bbed28ec3d2a820213ad3c
Rampal-09 <rampaltanwar0929@gmail.com>
2026-07-20 13:45:53 +0530

    Setup Express server and MongoDB connection

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+dotenv.config();
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongooDb Connected ");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
